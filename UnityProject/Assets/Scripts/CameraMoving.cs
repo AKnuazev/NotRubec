031e739d10caa454e94396c5d42144620aa2f205
CEZAR <Stavropoltsev.t@gmail.com>
2020-09-08 01:34:16 +0300

    added cube moving

---
+++ UnityProject/Assets/Scripts/CameraMoving.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoving : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 10f);
+    }
+}
---
+++ UnityProject/Assets/Scripts/CubeMoving.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMoving : MonoBehaviour
+{
+    private Vector3 forwardRotationPoint;
+    private Vector3 backRotationPoint;
+    private Vector3 leftRotationPoint;
+    private Vector3 rightRotationPoint;
+
+    private Bounds bounds;
+    private bool rolling;
+
+    public float speed;
+
+    void Start()
+    {
+        bounds = GetComponent<MeshRenderer>().bounds;
+
+        forwardRotationPoint = new Vector3(0, -bounds.extents.y, bounds.extents.z);
+        backRotationPoint = new Vector3(0, -bounds.extents.y, -bounds.extents.z);
+        leftRotationPoint = new Vector3(-bounds.extents.x, -bounds.extents.y, 0);
+        rightRotationPoint = new Vector3(bounds.extents.x, -bounds.extents.y, 0);
+    }
+
+    void Update()
+    {
+
+        if (rolling)
+            return;
+
+        if (Input.GetKey("up"))
+        {
+            StartCoroutine(move(forwardRotationPoint));
+        }
+        else if (Input.GetKey("down"))
+        {
+            StartCoroutine(move(backRotationPoint));
+        }
+        else if (Input.GetKey("left"))
+        {
+            StartCoroutine(move(leftRotationPoint));
+        }
+        else if (Input.GetKey("right"))
+        {
+            StartCoroutine(move(rightRotationPoint));
+        }
+    }
+
+    IEnumerator move(Vector3 rotationPoint)
+    {
+        Vector3 point = transform.position + rotationPoint;
+        Vector3 axis = Vector3.Cross(Vector3.up, rotationPoint).normalized;
+        float angle = 90;
+        float a = 0;
+
+        rolling = true;
+
+        while (angle > 0)
+        {
+            a = Time.deltaTime * speed;
+            transform.RotateAround(point, axis, a);
+            angle -= a;
+            yield return null;
+        }
+        transform.RotateAround(point, axis, angle);
+
+        rolling = false;
+    }
+
+}
