4d41ed33c440dadd2e6ebbfefd13fee17612230c
CEZAR <Stavropoltsev.t@gmail.com>
2020-09-09 01:33:42 +0300

    triger test

---
+++ UnityProject/Assets/Scripts/TriggerCheck.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCheck : MonoBehaviour
+{
+    GameObject player;
+    private void OnTriggerEnter(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = new Color(160,160,20);
+    }
+}
