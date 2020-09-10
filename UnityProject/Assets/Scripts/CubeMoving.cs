de44064dbd3bea7a749ca6e6a71b07635cac5500
CEZAR <Stavropoltsev.t@gmail.com>
2020-09-10 22:27:50 +0300

    return player back script

--- UnityProject/Assets/Scripts/CubeMoving.cs
+++ UnityProject/Assets/Scripts/CubeMoving.cs
@@ -5,53 +5,81 @@ using UnityEngine;
 
 public class CubeMoving : MonoBehaviour
 {
-    private Vector3 forwardRotationPoint;
-    private Vector3 backRotationPoint;
-    private Vector3 leftRotationPoint;
-    private Vector3 rightRotationPoint;
-
-    private Bounds bounds;
-    private bool rolling;
+    private Vector3[] rotationPoints = new Vector3[4]; // 0 - forward
+                                                       // 1 - back
+                                                       // 2 - left
+                                                       // 3 - right
+    //private Vector3 forwardRotationPoint;
+    //private Vector3 backRotationPoint;
+    //private Vector3 leftRotationPoint;
+    //private Vector3 rightRotationPoint;
 
     public float speed;
     public float delay;
 
-    //[NonSerialized]
-    public static Vector3 previousPos;
+    private Bounds bounds;
+
+    [SerializeField]
+    public Vector3 previousPos;
+
+    private bool rolling;
+
+    [SerializeField]
+    public bool movementFreeze; // 1 - player cant move
 
     void Start()
     {
         bounds = GetComponent<MeshRenderer>().bounds;
 
-        forwardRotationPoint = new Vector3(0, -bounds.extents.y, bounds.extents.z);
-        backRotationPoint = new Vector3(0, -bounds.extents.y, -bounds.extents.z);
-        leftRotationPoint = new Vector3(-bounds.extents.x, -bounds.extents.y, 0);
-        rightRotationPoint = new Vector3(bounds.extents.x, -bounds.extents.y, 0);
+        //forwardRotationPoint = new Vector3(0, -bounds.extents.y, bounds.extents.z);
+        //backRotationPoint = new Vector3(0, -bounds.extents.y, -bounds.extents.z);
+        //leftRotationPoint = new Vector3(-bounds.extents.x, -bounds.extents.y, 0);
+        //rightRotationPoint = new Vector3(bounds.extents.x, -bounds.extents.y, 0);
+
+        rotationPoints[0] = new Vector3(0, -bounds.extents.y, bounds.extents.z);
+        rotationPoints[1] = new Vector3(0, -bounds.extents.y, -bounds.extents.z);
+        rotationPoints[2] = new Vector3(-bounds.extents.x, -bounds.extents.y, 0);
+        rotationPoints[3] = new Vector3(bounds.extents.x, -bounds.extents.y, 0);
 
         previousPos = transform.position;
+        rolling = false;
+        movementFreeze = false;
+    }
+
+    public void startMoving(int direction)
+    {
+        previousPos = transform.position;
+        StartCoroutine(move(rotationPoints[direction]));
     }
 
     void Update()
     {
-
         if (rolling)
             return;
 
-        if (Input.GetKey("up"))
+        if (Input.GetKey("up") && movementFreeze == false)
         {
-            StartCoroutine(move(forwardRotationPoint));
+            //StartCoroutine(move(forwardRotationPoint));
+            //startMoving(forwardRotationPoint);
+            startMoving(0);
         }
-        else if (Input.GetKey("down"))
+        else if (Input.GetKey("down") && movementFreeze == false)
         {
-            StartCoroutine(move(backRotationPoint));
+            //StartCoroutine(move(backRotationPoint));
+            //startMoving(backRotationPoint);
+            startMoving(1);
         }
-        else if (Input.GetKey("left"))
+        else if (Input.GetKey("left") && movementFreeze == false)
         {
-            StartCoroutine(move(leftRotationPoint));
+            //StartCoroutine(move(leftRotationPoint));
+            //startMoving(leftRotationPoint);
+            startMoving(2);
         }
-        else if (Input.GetKey("right"))
+        else if (Input.GetKey("right") && movementFreeze == false)
         {
-            StartCoroutine(move(rightRotationPoint));
+            //StartCoroutine(move(rightRotationPoint));
+            //startMoving(rightRotationPoint);
+            startMoving(3);
         }
     }
 
@@ -73,8 +101,12 @@ public class CubeMoving : MonoBehaviour
         }
         transform.RotateAround(point, axis, angle);
 
-        previousPos = transform.position;
         yield return new WaitForSeconds(delay);
+
+        // round up player position
+        transform.position = new Vector3((float)Math.Round((double)transform.position.x, 1),
+                                    (float)Math.Round((double)transform.position.y, 1),
+                                    (float)Math.Round((double)transform.position.z, 1));
         rolling = false;
     }
 
--- UnityProject/Assets/Scripts/Triggers/ColorChecker.cs
+++ UnityProject/Assets/Scripts/Triggers/ColorChecker.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class ColorChecker : MonoBehaviour
 {
     public GameObject player;
+    public CubeMoving cubeMovingScript;
+
+    IEnumerator returnPlayer(int i)
+    {
+        yield return new WaitForSeconds(1);
+        cubeMovingScript.startMoving(i);
+        cubeMovingScript.movementFreeze = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "edges")
@@ -17,6 +26,32 @@ public class ColorChecker : MonoBehaviour
             else
             {
                 Debug.Log("Color NOT matching. Current color: " + other.GetComponent<Renderer>().material.color + "Need color: " + transform.GetComponent<Renderer>().material.color);
+                // Searching a position to return the player to
+                float deltaX = transform.position.x - cubeMovingScript.previousPos.x;
+                float deltaZ = transform.position.z - cubeMovingScript.previousPos.z;
+                Debug.Log(deltaX + " " + deltaZ);
+
+                cubeMovingScript.movementFreeze = true;
+                if (deltaZ < 0) // up
+                {
+                    Debug.Log("Return player up. DeltaZ = " + deltaZ + " < 0");
+                    StartCoroutine(returnPlayer(0));
+                }
+                else if (deltaZ > 0) // down
+                {
+                    Debug.Log("Return player down. DeltaZ = " + deltaZ + " > 0");
+                    StartCoroutine(returnPlayer(1));
+                }
+                else if (deltaX > 0) // left
+                {
+                    Debug.Log("Return player left. DeltaX = " + deltaX + " > 0");
+                    StartCoroutine(returnPlayer(2));
+                }
+                else if (deltaX < 0) // right
+                {
+                    Debug.Log("Return player right. DeltaX = " + deltaX + " < 0");
+                    StartCoroutine(returnPlayer(3));
+                }
             }
         }
         else return;
